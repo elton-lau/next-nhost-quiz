3004e0963395c13a79bd94410ccb76406753a82c
Elton Lau <twilightlau94@gmail.com>
2022-05-19 18:48:38 +0800

    initialize app with nhost & mantine

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import { NhostNextProvider, NhostClient } from '@nhost/nextjs'
+import { NhostApolloProvider } from '@nhost/react-apollo'
+import { MantineProvider } from '@mantine/core';
+
+const nhost = new NhostClient({
+  backendUrl: process.env.NEXT_PUBLIC_NHOST_BACKEND
+})
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+    <NhostApolloProvider nhost={nhost}>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </NhostApolloProvider>
+  </NhostNextProvider>
 }
 
 export default MyApp
+
---
+++ pages/_document.js
@@ -0,0 +1,9 @@
+import { Html, Main, NextScript } from "next/document";
+import { createGetInitialProps } from '@mantine/next';
+
+const getInitialProps = createGetInitialProps();
+
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps
+}
